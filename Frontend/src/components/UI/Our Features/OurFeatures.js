c85d9f2cc59769b171148a3f622e01f2509bc7a6
manisha <manishaskr2002@gmail.com>
2023-06-23 15:09:37 +0530

    first commit

---
+++ Frontend/src/components/UI/Our Features/OurFeatures.js
@@ -0,0 +1,18 @@
+import classes from './OurFeatures.module.css';
+import Card from './Card';
+
+const OurFeatures = () => {
+    return (
+        <div className={`${classes.bg}`}>
+            <div className={`${classes.heading}`}>CREATE A DOCUMENT IN CLICK OF A BUTTON</div>
+            <div className={`${classes.subheading}`}>OUR FEATURES.</div>
+            <div className={`${classes.cards}`}>
+                <Card name='Create an offer letter' link='/offer_letter/details' />
+                <Card name='Create single page resume' link='/resume1/details' />
+                <Card name='Create two page resume' link='/resume2/details' />
+            </div>
+        </div>
+    );
+}
+
+export default OurFeatures;
